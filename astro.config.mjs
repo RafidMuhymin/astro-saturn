b6b340d6bc29ec9fc886b91830a0bb1da85ae177
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-02 22:54:01 +0600

    added server options

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,4 +36,26 @@ export default defineConfig({
      * If ‘directory’, Astro will generate a directory with a nested index.html file (ex: “/foo/index.html”) for each page.
      */
   },
+
+  /*
+   * Server Options
+   *
+   * Customize the Astro dev server, used by both `astro dev` and `astro preview`.
+   *
+   * The config options can be passed via an object or a function that returns an object dynamically based on the command run (“dev”, “preview”)
+   *
+   * Object: { port: 1234, host: true }
+   * Function: (command) => ({ port: command === 'dev' ? 3000 : 4000 })
+   */
+  server: {
+    host: false,
+    /*
+     * Set which network IP addresses the server should listen on (i.e. non-localhost IPs).
+     *
+     * false - do not expose on a network IP address
+     * true - listen on all addresses, including LAN and public addresses
+     * [custom-address] - expose on a network IP address at [custom-address] (ex: 192.168.0.1)
+     */
+    port: 3000 /* The port the server should listen on. If the given port is already in use, Astro will automatically try the next available port. */,
+  },
 });
