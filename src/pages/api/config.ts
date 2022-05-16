fc7c9f29b19c46c6af46318dce2612940e8889fd
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-16 20:31:06 +0600

    fixed HMR issue

--- src/pages/api/config.ts
+++ src/pages/api/config.ts
@@ -5,32 +5,50 @@ import { parseJSONRequestBody } from "../../utils/admin/utils";
 export const get = async () => ({ body: "" });
 
 export const post: APIRoute = async ({ params, request }) => {
-  const requestBody = await parseJSONRequestBody(request.body);
+  try {
+    const requestBody = await parseJSONRequestBody(request.body);
 
-  const changedConfigs = JSON.parse(requestBody);
+    const changedConfigs = JSON.parse(requestBody);
 
-  const configPath = "./src/data/adminConfigs.json";
+    const configPath = "./src/data/adminConfigs.json",
+      layoutPath = "./src/layouts/AdminLayout.astro";
 
-  const adminConfigs = JSON.parse(
-    await fs.promises.readFile(configPath, "utf-8")
-  );
+    const adminConfigs = JSON.parse(
+      await fs.promises.readFile(configPath, "utf-8")
+    );
 
-  const updatedConfigs = {
-    ...adminConfigs,
-    ...changedConfigs,
-  };
+    const updatedConfigs = {
+      ...adminConfigs,
+      ...changedConfigs,
+    };
 
-  await fs.promises.writeFile(
-    configPath,
-    JSON.stringify(updatedConfigs, null, 2)
-  );
+    await fs.promises.writeFile(
+      configPath,
+      JSON.stringify(updatedConfigs, null, 2)
+    );
 
-  const response = {
-    body: JSON.stringify({
-      message: "Updated configs",
-      success: true,
-    }),
-  };
+    // Force Astro to display changed content
+    await fs.promises.writeFile(
+      layoutPath,
+      await fs.promises.readFile(layoutPath)
+    );
 
-  return response;
+    const response = {
+      body: JSON.stringify({
+        message: "Updated configs",
+        success: true,
+      }),
+    };
+
+    return response;
+  } catch (error) {
+    const response = {
+      body: JSON.stringify({
+        message: error.message,
+        success: false,
+      }),
+    };
+
+    return response;
+  }
 };
