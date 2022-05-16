250dfc05501e9cf3421cded5f3ff4b376daa5aa5
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-17 00:01:45 +0600

    parse stringified boolean, numeric and null values

--- src/pages/api/config.ts
+++ src/pages/api/config.ts
@@ -6,10 +6,6 @@ export const get = async () => ({ body: "" });
 
 export const post: APIRoute = async ({ params, request }) => {
   try {
-    const requestBody = await parseJSONRequestBody(request.body);
-
-    const changedConfigs = JSON.parse(requestBody);
-
     const configPath = "./src/data/adminConfigs.json",
       layoutPath = "./src/layouts/AdminLayout.astro",
       panelPath = "./src/components/admin/AdminPanel.astro";
@@ -18,9 +14,37 @@ export const post: APIRoute = async ({ params, request }) => {
       await fs.promises.readFile(configPath, "utf-8")
     );
 
+    const requestBody = await parseJSONRequestBody(request.body);
+
+    const changedConfigs = JSON.parse(requestBody);
+
+    const parentKey = Object.keys(changedConfigs)[0],
+      childObject = changedConfigs[parentKey],
+      childKeys = Object.keys(childObject);
+
+    const filteredValues = Object.keys(childObject).map((key) => {
+      if (["", "null"].includes(childObject[key])) return null;
+
+      if (childObject[key] === "true") return true;
+
+      if (childObject[key] === "false") return false;
+
+      if (!isNaN(childObject[key])) return parseInt(childObject[key]);
+
+      return childObject[key];
+    });
+
+    const filteredConfigs = {};
+
+    childKeys.forEach((key, index) => {
+      filteredConfigs[key] = filteredValues[index];
+    });
+
     const updatedConfigs = {
       ...adminConfigs,
-      ...changedConfigs,
+      ...{
+        [parentKey]: filteredConfigs,
+      },
     };
 
     await fs.promises.writeFile(
@@ -35,10 +59,7 @@ export const post: APIRoute = async ({ params, request }) => {
     ]);
 
     const response = {
-      body: JSON.stringify({
-        message: "Updated configs",
-        success: true,
-      }),
+      body: JSON.stringify(updatedConfigs),
     };
 
     return response;
